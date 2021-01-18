2d840398ebd5c02473ad6994be5e1d20c04b62d2
Metin <karatepemetin539@hotmail.com>
2021-01-18 20:51:43 +0300

    Initial Commit

---
+++ LandMarkBook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LandMarkBook
+//
+//  Created by Macintosh HD on 18.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
