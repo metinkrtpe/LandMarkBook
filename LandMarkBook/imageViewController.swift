5d50e67eddf42a1309d07524cf1053ef3f824e5a
Metin <karatepemetin539@hotmail.com>
2021-01-18 22:12:21 +0300

    first commit

---
+++ LandMarkBook/imageViewController.swift
@@ -0,0 +1,29 @@
+//
+//  imageViewController.swift
+//  LandMarkBook
+//
+//  Created by Macintosh HD on 18.01.2021.
+//
+
+import UIKit
+
+class imageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
