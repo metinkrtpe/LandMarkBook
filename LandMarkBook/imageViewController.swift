18ba82295faf9f81606674c9c17c1fa8667c9787
Metin <karatepemetin539@hotmail.com>
2021-01-18 22:16:16 +0300

    Tableview kullanimi kodlar.

--- LandMarkBook/imageViewController.swift
+++ LandMarkBook/imageViewController.swift
@@ -8,22 +8,18 @@
 import UIKit
 
 class imageViewController: UIViewController {
-
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var selectedlandmarkName = ""
+    var selectedlandmarkImage = UIImage()
+    
+    @IBOutlet weak var landmarkLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        landmarkLabel.text = selectedlandmarkName
+        imageView.image = selectedlandmarkImage
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
